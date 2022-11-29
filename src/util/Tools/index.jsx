cc3a4dd89bc8d5dd8414df2f51a463eb678dbe14
Genius <2683941980@qq.com>
2022-11-29 17:03:20 +0800

    数学计算

--- src/util/Tools/index.jsx
+++ src/util/Tools/index.jsx
@@ -22,9 +22,79 @@ const isArray = (array) => {
   return Array.isArray(array) ? array : [];
 };
 
+const decNum = (a) => { /* 获取小数位数 */
+  let r = 0;
+  if (a !== null && a !== undefined) {
+    a = a.toString();
+    if (a.indexOf('.') !== -1) r = a.split('.')[1].length;
+  }
+  return r;
+};
+
+const int = (a) => { /* 去除小数点并转成数值 */
+  if (a !== null && a !== undefined) {
+    if (Number(a) === 0) {
+      // eslint-disable-next-line radix
+      return parseInt('0');
+    } else {
+      // eslint-disable-next-line radix
+      return parseInt(a.toString().replace('.', ''));
+    }
+  } else {
+    // eslint-disable-next-line radix
+    return parseInt('0');
+  }
+};
+
+// 数学四则运算
+const MathCalc = (a, b, type) => {// 加减乘除
+  let r;
+  const da = decNum(a);
+  const db = decNum(b);
+  let dsum = da + db;
+  const dmin = Math.min(da, db);
+  let dmax = Math.max(da, db);
+  dsum += dmax - dmin;
+  // eslint-disable-next-line no-restricted-properties
+  dsum = Math.pow(10, dsum);
+  // eslint-disable-next-line no-restricted-properties
+  dmax = Math.pow(10, dmax);
+  a = int(a);
+  b = int(b);
+  if (da > db) {
+    // eslint-disable-next-line no-restricted-properties
+    b *= Math.pow(10, da - db);
+  } else {
+    // eslint-disable-next-line no-restricted-properties
+    a *= Math.pow(10, db - da);
+  }
+
+  switch (type) {
+    case 'jia':
+      r = (a + b) / dmax;
+      break;
+    case 'jian':
+      r = (a - b) / dmax;
+      break;
+    case 'cheng':
+      r = (a * b) / dsum;
+      break;
+    case 'chu':
+      if (b === 0) {
+        break;
+      }
+      r = a / b;
+      break;
+    default:
+      break;
+  }
+  return Number(r.toFixed(2));
+};
+
 
 export {
   randomString,
   isObject,
   isArray,
+  MathCalc,
 };
