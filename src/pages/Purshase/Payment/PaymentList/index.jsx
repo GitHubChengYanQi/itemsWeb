735a0c476308eb4ba0ef4926053cad6c328e6ac2
Jin <2531515632@qq.com>
2022-12-01 08:14:18 +0800

    付款

--- src/pages/Purshase/Payment/PaymentList/index.jsx
+++ src/pages/Purshase/Payment/PaymentList/index.jsx
@@ -7,7 +7,7 @@
 
 import React, {useRef, useState} from 'react';
 import {createFormActions} from '@formily/antd';
-import {Button} from 'antd';
+import {Button,message} from 'antd';
 import Table from '@/components/Table';
 import Drawer from '@/components/Drawer';
 import AddButton from '@/components/AddButton';
@@ -19,17 +19,18 @@ import {useRequest} from '@/util/Request';
 const formActionsPublic = createFormActions();
 
 const PaymentList = () => {
-
   const ref = useRef(null);
   const tableRef = useRef(null);
 
-  const {loading, run} = useRequest({
+  const {run,fetches} = useRequest({
     url: '/paymentRecord/obsolete',
     method: 'POST',
   }, {
     manual: true,
-    onSuccess: (result, params) => {
-
+    fetchKey: ({data: {recordId}}) => recordId,
+    onSuccess: () => {
+        message.success('作废成功');
+        tableRef.current.refresh();
     },
     onError: () => {
 
@@ -45,21 +46,23 @@ const PaymentList = () => {
       </>
     );
   };
+
   const columns = [
     {dataIndex: 'paymentAmount', title: '金额(人民币)'},
     {dataIndex: 'coding', title: '关联订单'},
     {dataIndex: 'createTime', title: '创建时间'},
     {dataIndex: 'remark', title: '备注'},
     {
-      dataIndex: 'orderId', title: '操作', render: (value, record,index) => {
+      dataIndex: 'orderId', title: '操作', render: (value, record) => {
         return <>
           <EditButton onClick={() => {
             ref.current.open(record.recordId);
           }}/>
-          <Button loading={loading} type='link' danger onClick={() => {
-            run({data: {recordId: record.recordId}});
-          }}>作废</Button>
-
+          <Button disabled={record.status === 50} loading={fetches[record.recordId]?.loading} type='link' danger
+                  onClick={() => {run({data: {recordId: record.recordId}});}}
+          >
+            {fetches[{data: {recordId: record.recordId}}]?.loading ? 'loading' : '作废'}
+          </Button>
         </>;
       }
     },
@@ -68,6 +71,7 @@ const PaymentList = () => {
   return (
     <>
       <Table
+        noTableColumnSet
         columns={columns}
         listHeader={false}
         cardHeaderStyle={{display: 'none'}}
