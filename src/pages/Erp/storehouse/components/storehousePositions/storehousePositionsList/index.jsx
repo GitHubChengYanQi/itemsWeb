5d53b0173a22ed59594d3de74bb565e8dc71cc0d
Genius <2683941980@qq.com>
2022-12-01 09:54:03 +0800

    库位绑定物料问题

--- src/pages/Erp/storehouse/components/storehousePositions/storehousePositionsList/index.jsx
+++ src/pages/Erp/storehouse/components/storehousePositions/storehousePositionsList/index.jsx
@@ -22,10 +22,12 @@ import Code from '@/pages/Erp/spu/components/Code';
 import {useRequest} from '@/util/Request';
 import TableSort from '@/components/Table/components/TableSort';
 import UpdateSort from '@/components/Table/components/UpdateSort';
-import StorehousePositionsBindList from '@/pages/Erp/storehouse/components/storehousePositionsBind/storehousePositionsBindList';
+import StorehousePositionsBindList
+  from '@/pages/Erp/storehouse/components/storehousePositionsBind/storehousePositionsBindList';
 import DeptTree from '@/components/DeptTree';
 import Import from '@/pages/Erp/sku/SkuTable/Import';
 import {config} from 'ice';
+import Note from '@/components/Note';
 
 const {baseURI} = config;
 
@@ -71,7 +73,7 @@ const StorehousePositionsList = (props) => {
       <Space>
         <AddButton onClick={() => {
           ref.current.open(false);
-        }}/>
+        }} />
       </Space>
     );
   };
@@ -94,28 +96,27 @@ const StorehousePositionsList = (props) => {
 
   return (
     <>
-      <Card title={<Breadcrumb title="仓库库位"/>} extra={actions()}>
-        <Space>
-          <Input placeholder="库位名称" onChange={(value) => {
-            setName(value.target.value);
-          }}/>
-          <Button htmlType='submit' type="primary" onClick={() => {
-            run({
-              params: {
-                name,
-              }
-            });
-          }}><SearchOutlined/>查询</Button>
-          <UpdateSort
-            disabled={sorts.length === 0}
-            type="positions"
-            sorts={sorts}
-            success={() => {
-              refresh();
-              setSorts([]);
-            }}/>
-        </Space>
-        <Divider/>
+      <Card title={<Space>
+        <Input placeholder="库位名称" onChange={(value) => {
+          setName(value.target.value);
+        }} />
+        <Button htmlType="submit" type="primary" onClick={() => {
+          run({
+            params: {
+              name,
+            }
+          });
+        }}><SearchOutlined />查询</Button>
+        <UpdateSort
+          disabled={sorts.length === 0}
+          type="positions"
+          sorts={sorts}
+          success={() => {
+            refresh();
+            setSorts([]);
+          }}
+        />
+      </Space>} extra={actions()}>
         <AntTable
           dataSource={data && data.map((item) => {
             return dataSourcedChildren(item);
@@ -124,18 +125,18 @@ const StorehousePositionsList = (props) => {
           rowKey="value"
           pagination={false}
         >
-          <Column title="库位名称" width={200} dataIndex="label" render={(value, record) => {
+          <Column title="库位名称" dataIndex="label" render={(value, record) => {
             return (
-              <>
-                <Code style={{width: 24, height: 24}} source="storehousePositions" id={record.value}/>
-                {value}
-              </>
+              <Space>
+                <Code style={{width: 24, height: 24}} source="storehousePositions" id={record.value} />
+                <Note width={200}>{value}</Note>
+              </Space>
             );
-          }}/>
+          }} />
           <Column
             title="序号"
             dataIndex="sort"
-            width={50}
+            width={100}
             align="center"
             render={(text, item) => {
               return <TableSort
@@ -144,8 +145,8 @@ const StorehousePositionsList = (props) => {
                 value={text || 0}
                 onChange={(value) => {
                   setSorts(value);
-                }}/>;
-            }}/>
+                }} />;
+            }} />
           <Column title="操作" align="right" render={(value, record) => {
             if (record.value !== '0') {
               return (
@@ -165,24 +166,24 @@ const StorehousePositionsList = (props) => {
                   }}>设置权限</Button>
                   <EditButton onClick={() => {
                     ref.current.open(record.value);
-                  }}/>
+                  }} />
                   <DelButton api={storehousePositionsDelete} value={record.value} onSuccess={() => {
                     refresh();
-                  }}/>
+                  }} />
                 </>
               );
             }
-          }}/>
+          }} />
         </AntTable>
       </Card>
       <Drawer width={800} title="编辑" component={StorehousePositionsEdit} onSuccess={() => {
         refresh();
         ref.current.close();
-      }} ref={ref} storehouse={value}/>
+      }} ref={ref} storehouse={value} />
 
       <Drawer width={800} title="物料信息" component={StorehousePositionsBindList} onSuccess={() => {
         bindRef.current.close();
-      }} ref={bindRef}/>
+      }} ref={bindRef} />
 
       <AntDrawer
         open={deptVisible}
@@ -194,7 +195,7 @@ const StorehousePositionsList = (props) => {
           setDepts([]);
         }}
       >
-        <DeptTree value={depts} onChange={setDepts}/>
+        <DeptTree value={depts} onChange={setDepts} />
         <div style={{textAlign: 'center', margin: 16}}>
           <Space>
             <Button disabled={depts.length === 0} type="primary" onClick={() => {
