60e21a0eb67933eaeec443a2a8ac92f45b273bcd
Genius <2683941980@qq.com>
2023-04-10 15:21:31 +0800

    空间修改

--- src/pages/BaseSystem/networkDisk/index.jsx
+++ src/pages/BaseSystem/networkDisk/index.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react';
-import {Button, Typography, Popover} from 'antd';
+import {Button, Typography, Popover, Spin} from 'antd';
 import {PlusOutlined} from '@ant-design/icons';
 import Breadcrumb from '@/components/Breadcrumb';
 import styles from './index.module.less';
@@ -7,8 +7,17 @@ import Modal from '@/components/Modal';
 import TranserUsers from '@/pages/BaseSystem/user/components/TranserUsers';
 import Add from '@/pages/BaseSystem/networkDisk/Add';
 import Table from '@/components/Table';
-import {detailUrl, listUrl} from '@/pages/BaseSystem/networkDisk/url';
+import {
+  detailUrl,
+  getUserResultByOpenIds,
+  listUrl,
+  rename,
+  spaceAclAdd,
+  spaceAclDel
+} from '@/pages/BaseSystem/networkDisk/url';
 import {useRequest} from '@/util/Request';
+import Message from '@/components/Message';
+import {isArray} from '@/util/Tools';
 
 const NetworkDisk = () => {
 
@@ -20,11 +29,44 @@ const NetworkDisk = () => {
 
   const [openPopover, setOpenPopover] = useState(false);
 
+  const [newAuth, setNewAuth] = useState([]);
+
+  const {loading: getUsersLoading, data: users = [], run: getUsers} = useRequest(getUserResultByOpenIds, {
+    manual: true,
+    onSuccess: (res) => {
+      setNewAuth(res.map(item => item.userId));
+    }
+  });
+
   const {loading: detailLoading, run: detailRun} = useRequest(detailUrl, {
     manual: true,
     onSuccess: (res) => {
-      console.log(res);
+      getUsers({
+        data: {
+          openIds: isArray(res?.wxCpSpaceInfo?.spaceInfo?.authList?.authInfo).map(item => item.userId)
+        }
+      });
+      setOpenPopover(res.spaceId);
     }
+  });
+
+  const {run: renameRun} = useRequest(rename, {
+    manual: true,
+    onSuccess: () => {
+      Message.success('修改成功！');
+      listRef.current.refresh();
+    },
+    onError: () => {
+      Message.error('修改失败！');
+    }
+  });
+
+  const {loading: spaceAclAddLoading, run: spaceAclAddRun} = useRequest(spaceAclAdd, {
+    manual: true,
+  });
+
+  const {loading: spaceAclDelLoading, run: spaceAclDelRun} = useRequest(spaceAclDel, {
+    manual: true,
   });
 
   const columns = [
@@ -43,12 +85,17 @@ const NetworkDisk = () => {
       }
     },
     {
-      title: '空间名称', dataIndex: 'spaceName', render: (value) => {
+      title: '空间名称', dataIndex: 'spaceName', render: (value, record) => {
         return <Typography.Paragraph
           style={{display: 'inline-block', margin: '0 12px'}}
           editable={{
-            onChange: () => {
-              console.log();
+            onChange: (value) => {
+              renameRun({
+                data: {
+                  spaceId: record.spaceId,
+                  spaceName: value
+                }
+              });
             },
           }}
         >
@@ -57,14 +104,35 @@ const NetworkDisk = () => {
       }
     },
     {
-      title: '操作', width: 100, align: 'center', dataIndex: 'spaceId', render: (value) => {
+      title: '操作', width: 120, align: 'center', dataIndex: 'spaceId', render: (value) => {
         return <div>
           <Popover
             open={openPopover === value}
-            content={<>
-              <TranserUsers />
+            content={getUsersLoading ? <Spin /> : <>
+              <TranserUsers
+                value={newAuth}
+                onChange={setNewAuth}
+              />
               <div className={styles.save}>
-                <Button size="small" type="primary">保存</Button>
+                <Button
+                  loading={spaceAclAddLoading || spaceAclDelLoading}
+                  size="small"
+                  type="primary"
+                  onClick={async () => {
+                    const addUsers = newAuth.filter(item => !users.find(user => user.userId === item));
+                    const delUsers = users.filter(user => !newAuth.find(item => user.userId === item));
+
+                    if (addUsers.length > 0){
+                      await spaceAclAddRun({data: {spaceId: value, userIds: addUsers}});
+                    }
+                    if (delUsers.length > 0){
+                      await spaceAclDelRun({data: {spaceId: value, userIds: delUsers.map(item => item.userId)}});
+                    }
+
+                    Message.success('修改成功！');
+                    listRef.current.refresh();
+                    setOpenPopover(false);
+                  }}>保存</Button>
               </div>
             </>}
             title="设置空间权限"
@@ -101,6 +169,7 @@ const NetworkDisk = () => {
         }}><PlusOutlined /> 添加</Button>
       </div>
       <Table
+        noTableColumnSet
         noRowSelection
         tableBorder
         ref={listRef}
--- src/pages/BaseSystem/networkDisk/url.js
+++ src/pages/BaseSystem/networkDisk/url.js
@@ -12,3 +12,30 @@ export const detailUrl = {
   url: '/wxWedriveSpace/detail',
   method: 'POST'
 };
+
+export const spaceAclAdd = {
+  url: '/wxWedriveSpace/spaceAclAdd',
+  method: 'POST'
+};
+
+export const spaceAclDel = {
+  url: '/wxWedriveSpace/spaceAclDel',
+  method: 'POST'
+};
+
+export const rename = {
+  url: '/wxWedriveSpace/rename',
+  method: 'POST'
+};
+
+export const wxWedriveSpaceDelete = {
+  url: '/wxWedriveSpace/delete',
+  method: 'POST',
+  rowKey:'spaceId'
+};
+
+
+export const getUserResultByOpenIds = {
+  url: '/mgr/getUserResultByOpenIds',
+  method: 'POST'
+};
